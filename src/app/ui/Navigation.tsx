3f69e8c400069f2d580d3d21041f97f6d4a26543
lliiiill <130821086+lliiiill@users.noreply.github.com>
2023-08-24 11:12:13 +0800

    Update Navigation.tsx

--- src/app/ui/Navigation.tsx
+++ src/app/ui/Navigation.tsx
@@ -1,16 +1,1 @@
-"use client";
 
-import React, { useState } from "react";
-import {
-  BgColorsOutlined,
-  ExperimentOutlined,
-  ThunderboltOutlined,
-  GithubOutlined,
-  ToolOutlined,
-} from "@ant-design/icons";
-import type { MenuProps } from "antd";
-import { Menu, Row, Col } from "antd";
-import { usePathname } from "next/navigation";
-import Link from "next/link";
-  );
-}
